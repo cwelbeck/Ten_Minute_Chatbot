83f935e8df8e2a7582e292a13c10ec51a4885795
cwelbeck <cwelbeck@gmail.com>
2020-09-02 21:02:00 -0500

    Ten_Minute_Chatbot

---
+++ _init_.py
@@ -0,0 +1,3 @@
+import os
+import sys
+sys.path.inset(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '...')))
---
+++ config.py
@@ -0,0 +1,3 @@
+HOST = "tenminuteqna.azurewebsites.net";
+ENDPOINT_KEY = "01ab2e9e-fc3e-454c-b5c7-247308b29b26";
+ROUTE = "/qnamaker/knowledgebases/e8427193-48a6-4909-905f-fa89a9c5a2fd/generateAnswer";
